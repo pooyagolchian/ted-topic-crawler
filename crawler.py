010cdbb9f6a02b12493b676a58d6f02eed04d709
Pooya Golchian <pooya.golchian@gmail.com>
2018-02-11 16:08:24 +0330

    add all dynamic change to var

--- crawler.py
+++ crawler.py
@@ -6,12 +6,14 @@ import lxml
 local_path = '/home/pooya/files/doc/o/'
 path_format = '.txt'
 html_selector ='div.media__message a.ga-link'
+ted_url = "http://www.ted.com/talks?page="
+ted_url_etc = "&topics%5B%5D="
 page_num = range(1,10)
 with open('data.txt', 'r') as headers:
     header = headers.read().splitlines()
     for h in header:
         for count in page_num:
-            urlPage = "http://www.ted.com/talks?page=" + str(count) + "&topics%5B%5D=" + h
+            urlPage = ted_url + str(count) + ted_url_etc + h
             response = requests.get(urlPage)
             if response:
                 data = response.text
